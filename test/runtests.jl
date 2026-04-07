1397051ae87e5a99b309718788922da38fbd6f7f
Kyle Beggs <beggskw@gmail.com>
2026-04-07 16:17:25 -0400

    refactor(test): replace @eval try/catch with Base.find_package for MTK tests
    Move availability check to runtests.jl so test_mtk_ext.jl is a plain
    test file consistent with test_scimlbase_ext.jl.

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,9 @@ using Test
     include("test_torord.jl")
     include("test_torord_correctness.jl")
     include("test_scimlbase_ext.jl")
-    include("test_mtk_ext.jl")
+    if Base.find_package("MTKCardiacCellModels") !== nothing
+        include("test_mtk_ext.jl")
+    else
+        @warn "MTKCardiacCellModels not available, skipping MTK extension tests"
+    end
 end
--- test/test_mtk_ext.jl
+++ test/test_mtk_ext.jl
@@ -1,54 +1,44 @@
-mtk_available = try
-    @eval using MTKCardiacCellModels
-    @eval using ModelingToolkit
-    true
-catch
-    false
-end
+using MTKCardiacCellModels
+using ModelingToolkit
+using OrdinaryDiffEq
 
-if mtk_available
-    using OrdinaryDiffEq
-
-    @testset "MTKCardiacCellModels Extension" begin
-        @testset "BeelerReuter model creation" begin
-            model = BeelerReuter()
-            @test num_states(model) == 8
-            @test num_parameters(model) > 0
-            @test has_symbolic_system(model)
-            @test symbolic_system(model) !== nothing
-            @test transmembrane_potential_index(model) > 0
-        end
-
-        @testset "BeelerReuter interface compliance" begin
-            model = BeelerReuter()
-            u0 = default_initial_state(model)
-            @test length(u0) == num_states(model)
-            @test length(state_names(model)) == num_states(model)
-            @test length(parameter_names(model)) == num_parameters(model)
-        end
-
-        @testset "BeelerReuter functor" begin
-            model = BeelerReuter()
-            u0 = default_initial_state(model)
-            du = similar(u0)
-            model(du, u0, nothing, 0.0)
-            @test all(isfinite, du)
-        end
-
-        @testset "BeelerReuter ODEProblem + solve" begin
-            model = BeelerReuter()
-            prob = ODEProblem(model, (0.0, 100.0))
-            sol = solve(prob, Tsit5())
-            @test sol.retcode == ReturnCode.Success
-            @test all(isfinite, sol.u[end])
-        end
-
-        @testset "BeelerReuter caching" begin
-            m1 = BeelerReuter()
-            m2 = BeelerReuter()
-            @test m1 === m2
-        end
+@testset "MTKCardiacCellModels Extension" begin
+    @testset "BeelerReuter model creation" begin
+        model = BeelerReuter()
+        @test num_states(model) == 8
+        @test num_parameters(model) > 0
+        @test has_symbolic_system(model)
+        @test symbolic_system(model) !== nothing
+        @test transmembrane_potential_index(model) > 0
     end
-else
-    @warn "MTKCardiacCellModels not available, skipping MTK extension tests"
+
+    @testset "BeelerReuter interface compliance" begin
+        model = BeelerReuter()
+        u0 = default_initial_state(model)
+        @test length(u0) == num_states(model)
+        @test length(state_names(model)) == num_states(model)
+        @test length(parameter_names(model)) == num_parameters(model)
+    end
+
+    @testset "BeelerReuter functor" begin
+        model = BeelerReuter()
+        u0 = default_initial_state(model)
+        du = similar(u0)
+        model(du, u0, nothing, 0.0)
+        @test all(isfinite, du)
+    end
+
+    @testset "BeelerReuter ODEProblem + solve" begin
+        model = BeelerReuter()
+        prob = ODEProblem(model, (0.0, 100.0))
+        sol = solve(prob, Tsit5())
+        @test sol.retcode == ReturnCode.Success
+        @test all(isfinite, sol.u[end])
+    end
+
+    @testset "BeelerReuter caching" begin
+        m1 = BeelerReuter()
+        m2 = BeelerReuter()
+        @test m1 === m2
+    end
 end
